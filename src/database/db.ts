993224bd7687e5ffa7c9750438b2cc249179474c
MuhammadRehanQadri <rehan.code@gmail.com>
2025-01-17 10:29:15 +0400

    ref(db-uri): env can define the mongo db uri

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/');
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/');
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Failed to connect to MongoDB', err);
